4be1cce458bb5e6ceed55bed63c021b93d75fb9c
Sylvain <sylvain@buyco.co>
2021-07-02 14:50:24 +0200

    add config for api

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,35 +1,55 @@
-import * as React from 'react'
-import OpenAPIClientAxios from 'openapi-client-axios'
-import { Client } from './gen/client'
-import definition from './gen/schema.json'
+import * as React from "react";
+import OpenAPIClientAxios from "openapi-client-axios";
+import { Client } from "./gen/client";
+import definition from "./gen/schema.json";
 
 interface ApiContextState {
-  client: Client | undefined
+  client: Client | undefined;
 }
 
 const ApiContext = React.createContext<ApiContextState>({
   client: undefined,
-})
+});
 
-const api = new OpenAPIClientAxios({
-  /* @ts-ignore */
-  definition,
-})
+interface ApiProviderProps {
+  url: string;
+  token: string;
+}
 
-export const ApiProvider: React.FC = ({ children }) => {
-  const client = api.initSync<Client>()
+export const ApiProvider: React.FC<ApiProviderProps> = ({
+  url,
+  token,
+  children,
+}) => {
+  const apiRef = React.useRef(
+    new OpenAPIClientAxios({
+      /* @ts-ignore */
+      definition,
+      withServer: { url },
+      axiosConfigDefaults: {
+        headers: {
+          common: {
+            "X-SESSION": token,
+          },
+        },
+      },
+    }),
+  );
+  const clientRef = React.useRef(apiRef.current.initSync<Client>());
 
   return (
-    <ApiContext.Provider value={{ client }}>{children}</ApiContext.Provider>
-  )
-}
+    <ApiContext.Provider value={{ client: clientRef.current }}>
+      {children}
+    </ApiContext.Provider>
+  );
+};
 
 export const useApi = () => {
-  const { client } = React.useContext(ApiContext)
+  const { client } = React.useContext(ApiContext);
 
   if (!client) {
-    throw new Error('A client API must be defined')
+    throw new Error("A client API must be defined");
   }
 
-  return client
-}
+  return client;
+};
