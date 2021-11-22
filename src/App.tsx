86ea71f796f2bc61ca48a3bc42ed34932a0cacc6
Luiz Tanure <letanure@gmail.com>
2021-11-22 10:50:05 -0300

    chore: reorganise folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import InvoicesList from './components/InvoicesList'
-import InvoiceShow from './components/InvoiceShow'
-
-import GettingStarted from './GettingStarted'
+import InvoicesList from './components/Invoice/InvoicesList'
+import InvoiceShow from './components/Invoice/InvoiceShow'
 
 function App() {
   return (
     <div className="px-5">
-      <GettingStarted />
       <Router>
         <Switch>
           <Route path="/invoice/:id" component={InvoiceShow} />
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { ApiProvider } from '../api'
+import { ApiProvider } from './api'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
