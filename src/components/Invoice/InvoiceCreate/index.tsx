3e7f33d62010aa63d49ecc216718b848551f8eb9
Luiz Tanure <letanure@gmail.com>
2021-11-26 00:06:20 +0100

    feat: add form create and save data

--- src/components/Invoice/InvoiceCreate/index.tsx
+++ src/components/Invoice/InvoiceCreate/index.tsx
@@ -1,9 +1,55 @@
-export type InvoiceCreateProps = {
-  title?: string
-}
+import { Paths } from 'api/gen/client'
+import { useApi } from 'api'
+import InvoiceForm from 'components/Invoice/InvoiceForm'
 
-const InvoiceCreate = ({ title = 'TITLE' }: InvoiceCreateProps) => {
-  return <h1>InvoiceCreate {title}</h1>
+const InvoiceCreate = () => {
+  const api = useApi()
+
+  const handleSubmit = ({
+    customer_id,
+    finalized,
+    paid,
+    date,
+    deadline,
+  }: any) => {
+    console.log('handleSubmit', {
+      customer_id,
+      finalized,
+      paid,
+      date,
+      deadline,
+    })
+    const dataInvoice: Paths.PostInvoices.RequestBody['invoice'] = {
+      customer_id,
+      finalized,
+      paid,
+      date,
+      deadline,
+      invoice_lines_attributes: [],
+    }
+    api
+      .postInvoices(null, {
+        invoice: dataInvoice,
+      })
+      .then(({ data }) => {
+        console.log(data)
+      })
+  }
+
+  const newInvoice = {
+    customer_id: null,
+    customer: undefined,
+    date: null,
+    deadline: null,
+    finalized: false,
+    id: 0,
+    invoice_lines: [],
+    paid: false,
+    tax: null,
+    total: null,
+  }
+
+  return <InvoiceForm data={newInvoice} onSubmit={handleSubmit} />
 }
 
 export default InvoiceCreate
