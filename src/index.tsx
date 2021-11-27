c5c02868b3cd806c73b0322af00cc4f6970bc761
Luiz Tanure <letanure@gmail.com>
2021-11-27 12:37:35 +0100

    feat: add i18n

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { ApiProvider } from './api'
 import env from './constants/env'
+import 'i18n'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApiProvider url={env.API_URL} token={env.API_TOKEN}>
-      <App />
-    </ApiProvider>
+    <Suspense fallback="Loading">
+      <ApiProvider url={env.API_URL} token={env.API_TOKEN}>
+        <App />
+      </ApiProvider>
+    </Suspense>
   </React.StrictMode>,
   document.getElementById('root')
 )
