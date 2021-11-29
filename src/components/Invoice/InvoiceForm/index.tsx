8cd81cdf4001f29196e1fa712afcad995fe873d9
Luiz Tanure <letanure@gmail.com>
2021-11-29 20:28:54 +0100

    refactor:  add translations to form labels

--- src/components/Invoice/InvoiceForm/index.tsx
+++ src/components/Invoice/InvoiceForm/index.tsx
@@ -2,68 +2,6 @@ import FormRender, { FormConfig } from 'components/ui/FormRender'
 import { Invoice, InvoiceCreatePayload } from 'types'
 import { useTranslation } from 'react-i18next'
 
-const formConfig: FormConfig = [
-  {
-    name: 'customer_id',
-    label: 'Customer',
-    type: 'CustomerAutocomplete',
-    placeholder: '',
-    required: true,
-    value: '',
-    valueType: 'number',
-  },
-  {
-    name: 'finalized',
-    label: 'Finalized?',
-    type: 'select',
-    placeholder: '',
-    required: true,
-    value: false,
-    options: [
-      { label: 'Yes', value: true },
-      { label: 'No', value: false },
-    ],
-    valueType: 'boolean',
-  },
-  {
-    name: 'paid',
-    label: 'Paid?',
-    type: 'select',
-    placeholder: '',
-    required: true,
-    value: false,
-    options: [
-      { label: 'Yes', value: true },
-      { label: 'No', value: false },
-    ],
-    valueType: 'boolean',
-  },
-  {
-    name: 'date',
-    label: 'Date',
-    type: 'Datepicker',
-    placeholder: '',
-    required: true,
-    value: '',
-  },
-  {
-    name: 'deadline',
-    label: 'Deadline',
-    type: 'Datepicker',
-    placeholder: '',
-    required: true,
-    value: '',
-  },
-  {
-    name: 'invoice_lines_attributes',
-    label: 'Products',
-    type: 'number',
-    placeholder: '',
-    required: false,
-    value: [],
-  },
-]
-
 export type InvoiceFormProps = {
   values: Invoice | null
   onSubmit: (data: InvoiceCreatePayload) => void
@@ -71,6 +9,67 @@ export type InvoiceFormProps = {
 
 const InvoiceForm = ({ values, onSubmit }: InvoiceFormProps) => {
   const { t } = useTranslation()
+  const formConfig: FormConfig = [
+    {
+      name: 'customer_id',
+      label: t('invoice.propLabel.customer'),
+      type: 'CustomerAutocomplete',
+      placeholder: '',
+      required: true,
+      value: '',
+      valueType: 'number',
+    },
+    {
+      name: 'finalized',
+      label: t('invoice.propLabel.finalized'),
+      type: 'select',
+      placeholder: '',
+      required: true,
+      value: false,
+      options: [
+        { label: 'Yes', value: true },
+        { label: 'No', value: false },
+      ],
+      valueType: 'boolean',
+    },
+    {
+      name: 'paid',
+      label: t('invoice.propLabel.paid'),
+      type: 'select',
+      placeholder: '',
+      required: true,
+      value: false,
+      options: [
+        { label: 'Yes', value: true },
+        { label: 'No', value: false },
+      ],
+      valueType: 'boolean',
+    },
+    {
+      name: 'date',
+      label: t('invoice.propLabel.date'),
+      type: 'Datepicker',
+      placeholder: '',
+      required: true,
+      value: '',
+    },
+    {
+      name: 'deadline',
+      label: t('invoice.propLabel.deadline'),
+      type: 'Datepicker',
+      placeholder: '',
+      required: true,
+      value: '',
+    },
+    {
+      name: 'invoice_lines_attributes',
+      label: t('invoice.propLabel.invoice_lines_attributes'),
+      type: 'number',
+      placeholder: '',
+      required: false,
+      value: [],
+    },
+  ]
   const handleSubmit = (data: any) => {
     onSubmit(data)
   }
